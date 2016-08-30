9847654da89b0b2edc038e4b678f9657b59e8be7
Vijay <aivijay@gmail.com>
2016-08-29 18:39:46 -0600

    Adding dependencies and cljsbuild

--- project.clj
+++ project.clj
@@ -5,7 +5,17 @@
             :url "http://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.datomic/datomic-free "0.9.5350"]
-                 [cheshire "5.6.3"]]:datomic {:schemas ["resources/datomic" ["schema.edn"]]}
+                 [cheshire "5.6.3"]
+                 [org.slf4j/slf4j-simple "1.7.5"]
+                 [org.clojure/data.json "0.2.2"]
+                 [me.raynes/fs "1.4.4"]
+                 [org.clojure/clojurescript "1.9.227"]
+                 [org.clojure/tools.namespace "0.2.11"]]
+  :datomic {:schemas ["resources/datomic" ["schema.edn"]]}
+  :cljsbuild {:builds [{:source-paths ["src-cljs"],
+                        :compiler {:pretty-printer true,
+                                   :output-to "www/js/main.js",
+                                   :optimizations :whitespace}}]}
   :profiles {:dev
              {:datomic {:config "resources/datomic/free-transactor-template.properties"
                         :db-uri "datomic:free://localhost:4334/conceptnet"}}})
