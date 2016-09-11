df2d6edcc271b235507ae5432a821963b9f3df20
Vijay <aivijay@gmail.com>
2016-09-11 16:07:43 -0600

    Adding some datalog util functions

---
+++ src/conceptnet_clj/datalog.clj
@@ -0,0 +1,12 @@
+(ns conceptnet-clj.datalog)
+
+(defn tuplify
+  "Returns a vector of the vals at keys ks in map."
+  [m ks]
+  (mapv #(get m %) ks))
+
+(defn maps->rel
+  "Convert a collection of maps into a relation, returning
+   the tuplification of each map by ks"
+  [maps ks]
+  (mapv #(tuplify % ks) maps))
